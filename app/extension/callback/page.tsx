a8b84d4341f15178fba1fe21583f38c455ca70cb
gio71165 <gio@searchfindr.net>
2025-12-18 16:45:12 -0500

    Add extension callback route

---
+++ app/extension/callback/page.tsx
@@ -0,0 +1,8 @@
+export default function ExtensionCallback() {
+  return (
+    <div style={{ padding: 40, fontFamily: "sans-serif" }}>
+      <h2>SearchFindr Extension</h2>
+      <p>You can close this tab and return to the extension.</p>
+    </div>
+  );
+}
